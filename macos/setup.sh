a6078e54f7dafcf4f86442f4b70a19ef14ca85a9
Andrew Volozhanin <scarfacedeb@gmail.com>
2023-01-29 12:41:30 +0100

    Add ruby and macos setup scripts

---
+++ macos/setup.sh
@@ -0,0 +1,6 @@
+
+
+# Disable App Nap for Anki to keep AnkiConnect API running
+defaults write net.ankiweb.dtop NSAppSleepDisabled -bool true
+defaults write net.ichi2.anki NSAppSleepDisabled -bool true
+defaults write org.qt-project.Qt.QtWebEngineCore NSAppSleepDisabled -bool true
