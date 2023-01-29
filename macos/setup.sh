f8a673b9702fd0dc3b0be0b89828510911fb7a2e
Andrew Volozhanin <scarfacedeb@gmail.com>
2023-01-29 17:03:15 +0100

    Add kitty and hammerspoon configs

--- macos/setup.sh
+++ macos/setup.sh
@@ -1,5 +1,3 @@
-
-
 # Disable App Nap for Anki to keep AnkiConnect API running
 defaults write net.ankiweb.dtop NSAppSleepDisabled -bool true
 defaults write net.ichi2.anki NSAppSleepDisabled -bool true
