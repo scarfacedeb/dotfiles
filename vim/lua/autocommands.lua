0e93094d929fdd3c009a7bcedee54adf86bc59a1
Andrew Volozhanin <scarfacedeb@gmail.com>
2023-10-22 23:01:32 +0200

    Remove nvim config

--- vim/lua/autocommands.lua
+++
@@ -1,9 +0,0 @@
-
--- Strip whitespace at the end of the lines
-local strip_line_exts = {
-  "*.py", "*.js", "*.rb", "*.css", "*.scss", 
-  "*.slim", "*.haml", "*.elm", 
-  "*.ex", "*.exs"
-}
-
-vim.cmd("autocmd BufWritePre " .. table.concat(strip_line_exts, ",") .. " :StripLines")
