8f7fbe5f49f4078eee5897530a53f7c112ff01e9
Andrew Volozhanin <scarfacedeb@gmail.com>
2026-04-23 14:45:45 +0200

    Switch back to solargraph

--- vim/config/lua/plugins/lsp.lua
+++ vim/config/lua/plugins/lsp.lua
@@ -25,11 +25,11 @@ return {
       -- Ensure these servers are installed; automatic_enable = true (default)
       -- means mason-installed servers are enabled automatically
       require('mason-lspconfig').setup({
-        ensure_installed = { 'ruby_lsp', 'ts_ls', 'eslint', 'gopls' },
+        ensure_installed = { 'solargraph', 'ts_ls', 'eslint', 'gopls' },
       })
 
       -- Disable servers auto-loaded from nvim-lspconfig runtimepath that we don't want
-      vim.lsp.enable('solargraph', false)
+      vim.lsp.enable('ruby_lsp', false)
     end,
   },
 }
