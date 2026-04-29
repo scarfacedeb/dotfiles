f21f27d85f95b0fd6f71c09f5203894586513242
Andrew Volozhanin <scarfacedeb@gmail.com>
2026-04-29 23:51:42 +0200

    Fix latest version fetch

--- ruby/setup.sh
+++ ruby/setup.sh
@@ -5,7 +5,7 @@ brew install chruby ruby-install
 
 # Fetch the latest stable Ruby version
 ruby-install --update > /dev/null
-LATEST_VERSION=$(ruby-install | awk '/  ruby:/{found=1; next} found{print $1; exit}')
+LATEST_VERSION=$(ruby-install | awk '/  ruby:/{found=1; next} found && /^  [a-z]/{exit} found && NF{last=$1} END{print last}')
 
 SCRIPT_DIR=${0:a:h}
 echo "ruby-$LATEST_VERSION" > "$SCRIPT_DIR/ruby-version"
